82f459e03000aa19540c2775160f9d99df6abf38
Lucia Cahojova <luciacahojova@Sansfil-Eduroam-Externe-208-186.polymtl.ca>
2022-09-20 15:30:41 -0400

    fix bad code

--- src/main/java/badcode/BadCode.java
+++ src/main/java/badcode/BadCode.java
@@ -4,15 +4,17 @@ public class BadCode {
 
     // Print the average and the sum of an array of integers
     public int PRINTsumandAVERAGE(int x[]) {
-        int sum;
+        int sum = 0;
         int average = 0;
 
         for (int i = 0; i < x.length; i++) {
-            average += x[i];
+            sum += x[i];
         }
 
-        sum = average / x.length;
-
+        if (x.length != 0){
+            average = sum / x.length;
+        }
+        
         System.out.println("Sum: " + sum);
         System.out.println("Average: " + average);
 
