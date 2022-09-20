69637d4a5ab938950cb8b932ddf52c21f33fc33b
Étienne <elescarbeault@gmail.com>
2022-09-20 15:21:19 -0400

    fixed sum and average calculation

--- src/main/java/badcode/BadCode.java
+++ src/main/java/badcode/BadCode.java
@@ -4,14 +4,14 @@ public class BadCode {
 
     // Print the average and the sum of an array of integers
     public int PRINTsumandAVERAGE(int x[]) {
-        int sum;
+        int sum = 0;
         int average = 0;
 
         for (int i = 0; i < x.length; i++) {
-            average += x[i];
+            sum += x[i];
         }
 
-        sum = average / x.length;
+        average = sum / x.length;
 
         System.out.println("Sum: " + sum);
         System.out.println("Average: " + average);
